0af7e910eca515018c31b0d3aa50e3a07f60eefe
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-02-25 09:04:30 +0000

    1,2,3, and hlaf 4 push

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const MainLayout = ({Children}) => {
+    return (
+    <div>
+     <Header/>
+     {Children}
+     <Footer/>
+     </div>
+    );
+}
+
+export default MainLayout
