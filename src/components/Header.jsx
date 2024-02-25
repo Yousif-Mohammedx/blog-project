b2f2c9476d1dc324eb3e5174a1a5f2ae63c2d2b9
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-02-25 15:22:16 +0000

    Blog-project - Commit 4 (responsive header)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,41 @@ import { images } from "../constants";
 
 const navItemsInfo = [
   { name: "Home", type: "link" },
-  { name: "Articles", type : "link" },
-  { name: "Pages", type : "dropdown" },
-  { name: "Pricing", type : "link" },
-  { name: "Faq", type : "link"},
+  { name: "Articles", type: "link" },
+  { name: "Pages", type: "dropdown", items: ["About us", "Contact Us"] },
+  { name: "Pricing", type: "link" },
+  { name: "Faq", type: "link" },
 ];
 
-const NavItem = ({ name }) => {
+const NavItem = ({ item }) => {
   return (
     <li className="relative group">
-      <a href="/" className="px-4 py-2">
-        {name}
-      </a>
-      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
+      {item.type === "link" ? (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
+        </>
+      ) : (
+        <>
+          <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+            <span>
+              {item.name}
+            </span>
+            <MdKeyboardArrowDown className="inline-block" />
+          </a>
+          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
+            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden bg-white">
+              {item.items.map((page) => (
+                <a href="/" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
+                  {page}
+                </a>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </li>
   );
 };
@@ -54,7 +76,7 @@ const Header = () => {
         >
           <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
             {navItemsInfo.map((item) => (
-              <NavItem key={item.name} name={item.name} />
+              <NavItem key={item.name} item={item} />
             ))}
           </ul>
           <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">Sign in</button>
