786ff69faa244e98fa3e0890756911e490317f78
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-02-25 16:03:49 +0000

    Blog Project (Video 5 done) making hero section & Responsive with the hero image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,15 @@ const navItemsInfo = [
 ];
 
 const NavItem = ({ item }) => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdownHandler = () => {
+    setDropdown((curState) => {
+      return !curState;
+    });
+  };
+
+
   return (
     <li className="relative group">
       {item.type === "link" ? (
@@ -19,39 +28,43 @@ const NavItem = ({ item }) => {
           <a href="/" className="px-4 py-2">
             {item.name}
           </a>
-          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
+          <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+            /
+          </span>
         </>
       ) : (
-        <>
-          <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
-            <span>
-              {item.name}
-            </span>
-            <MdKeyboardArrowDown className="inline-block" />
-          </a>
-          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
-            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden bg-white">
+        <div className="flex flex-col items-center">
+          <button
+            className="px-4 py-2 flex gap-x-1 items-center"
+            onClick={toggleDropdownHandler}
+          >
+            <span>{item.name}</span>
+            <MdKeyboardArrowDown />
+          </button>
+          <div
+            className={`${
+              dropdown ? "block" : "hidden"
+            } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+          >
+            <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
               {item.items.map((page) => (
-                <a href="/" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
-                  {page}
-                </a>
+                  <a href="/" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
+                    {page}
+                  </a>
               ))}
             </ul>
           </div>
-        </>
+        </div>
       )}
     </li>
   );
 };
 
-
 const Header = () => {
   const [navIsVisible, setNavIsVisible] = useState(false);
 
   const navVisibilityHandler = () => {
-    setNavIsVisible((curState) => {
-      return !curState;
-    });
+    setNavIsVisible((prevState) => !prevState);
   };
 
   return (
@@ -75,8 +88,8 @@ const Header = () => {
             } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
         >
           <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
-            {navItemsInfo.map((item) => (
-              <NavItem key={item.name} item={item} />
+            {navItemsInfo.map((item, index) => (
+              <NavItem key={index} item={item} />
             ))}
           </ul>
           <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">Sign in</button>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-import MainLayout from "../components/MainLayout";
+import MainLayout from "../../components/MainLayout";
+import Hero from "./container/Hero"
 
 const HomePage = () => {
   return (
     <MainLayout>
+      <Hero />
     </MainLayout>
   );
 };
