45b3f68dba4ab68284f212d5ffa2c68052fb7c14
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-02-28 07:01:43 +0000

    Video 7 (done)

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,11 +2,13 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from '../assets/HeroImage.svg'
 import Post1Image from "../assets/posts/post1.jpg";
 import Post1Profile from "../assets/posts/post-profile.png";
+import CtaImage from "../assets/CtaImage.jpg";
 const images = {
     Logo,
     HeroImage,
     Post1Image,
     Post1Profile,
+    CtaImage,
 };
 
 export default images;
