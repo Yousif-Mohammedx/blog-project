c4af6d70b5a5a7e93e60459b157080d9dbcf17af
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-02-29 04:40:22 +0000

    video 8 done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import {
-  FaFacebookF,
-  FaYoutube,
-  FaSquareInstagram,
-  FaTelegram,
-  FaXTwitter,
-} from "react-icons/fa6";
-import { images } from '../constants'
+  AiOutlineTwitter,
+  AiFillYoutube,
+  AiFillInstagram,
+  AiFillHeart,
+} from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
+import { BsTelegram } from "react-icons/bs";
+
+import { images } from "../constants";
 
 const Footer = () => {
   return (
@@ -16,7 +18,7 @@ const Footer = () => {
           <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
           <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>
-              <a href="/">Landing Page</a>
+              <a href="/">Landingpage</a>
             </li>
             <li>
               <a href="/">Features</a>
@@ -83,31 +85,54 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className='col-span-10'>
-          <img src={images.Logo} alt="Logo" />
-          <p>Build a modern and creative website with crealand</p>
-          <ul className='flex justify-center items-center mt-5 space-x-4 text-gray-300'></ul>
-          <li>
-            <a href="/">
-              <FaXTwitter className="w-6 h-auto"/>
-            </a>
-            <a href="/">
-              <FaFacebookF className="w-6 h-auto"/>
-            </a>
-            <a href="/">
-              <FaYoutube className="w-6 h-auto"/>
-            </a>
-            <a href="/">
-              <FaSquareInstagram className="w-6 h-auto"/>
-            </a>
-            <a href="/">
-              <FaTelegram className="w-6 h-auto"/>
-            </a>
-          </li>
+        <div className="col-span-10 md:order-first md:col-span-4 lg:col-span-2">
+          <img
+            src={images.Logo}
+            alt="logo"
+            className="brightness-0 invert mx-auto md:mx-0"
+          />
+          <p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base lg:text-sm">
+            Build a modern and creative website with crealand
+          </p>
+          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
+            <li>
+              <a href="/">
+                <AiOutlineTwitter className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <AiFillYoutube className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <AiFillInstagram className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <FaFacebook className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <BsTelegram className="w-6 h-auto" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden md:flex flex-col items-center space-y-4 md:col-span-12 lg:col-span-10">
+          <div className="bg-primary text-white p-3 rounded-full">
+            <AiFillHeart className="w-7 h-auto" />
+          </div>
+          <p className="font-bold italic text-dark-light">
+            Copyright © 2024. Yousif Sabba.
+          </p>
         </div>
       </footer>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
