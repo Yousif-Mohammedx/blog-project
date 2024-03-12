d5c8cfbd86cb3d5883e2c56cb60b8e55378c30d0
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-03-12 13:54:11 +0000

    10% video 20 (done)

---
+++ src/pages/register/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterPage = () => {
+    return (
+        <MainLayout>
+            <section className='container mx-auto px-5 py-10'></section>
+        </MainLayout>
+    )
+}
+
+export default RegisterPage;
