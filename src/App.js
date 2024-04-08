7ffc2183452eeddc4e6d8d9bf356b158f502fb14
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-04-08 10:36:19 +0000

    video 25

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import HomePage from './pages/home/HomePage';
 import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
 import RegisterPage from './pages/register/RegisterPage';
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import LoginPage from "./pages/login/LoginPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <Toaster />
     </div>
